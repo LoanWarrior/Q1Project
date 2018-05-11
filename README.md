792650ce96c76fa27d6a32953cdb745ba3828292
Edison <edisontoole@gmail.com>
2018-05-10 21:19:06 -0600

    mostly finished but will still add things. changed the parallax photos, added the secret page, changed capitaliztion on things, etc.

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# Q1 Project Proposals
-
-1. a site that lets you enter your location, and get shown all nearby options for parking on a map, like how much paid spaces run, where the lots are, where free parking is, timed parking, etc. using APIs for parking and maps.
-   (not sure if this is out of scope or if an API for parking would do this)
-
-2. a site that lets you enter two instagram names, and shows all sorts of connections they share. like people that follow both accounts, followed by both accounts, followed by one and following the other, etc.
-uses instagram api.
+# Q1 Project
+this is an app called pearl park, that doesn't actually work at all and is a front end development project. I designed it to be like a functional version of the site. 
--- q1.js
+++ q1.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('.parallax').parallax();
+  $('.sidenav').sidenav();
 
   mapboxgl.accessToken = 'pk.eyJ1IjoiZWRpc29udG9vbGUiLCJhIjoiY2pncXdnajM2MGg2ejJ4cGUzdW92bDNzcCJ9.YG4_JLO78bqmlpBcLHzuWw';
   var map = new mapboxgl.Map({
@@ -11,9 +12,7 @@ $(document).ready(function() {
   var geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken
   });
-  map.addControl(new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken
-  }));
+  map.addControl(geocoder);
   map.addControl(new mapboxgl.GeolocateControl({
     positionOptions: {
       enableHighAccuracy: true
@@ -23,15 +22,15 @@ $(document).ready(function() {
   map.addControl(new mapboxgl.FullscreenControl());
 
 
-  // document.getElementById('info').appendChild(geocoder.onAdd('map'));
-  // map.on('mousemove', function(e) {
-  //   document.getElementById('info').innerHTML =
-  //     // e.point is the x, y coordinates of the mousemove event relative
-  //     // to the top-left corner of the map
-  //     JSON.stringify(e.point) + '<br />' +
-  //     // e.lngLat is the longitude, latitude geographical position of the event
-  //     JSON.stringify(e.lngLat);
-  // });
+  document.getElementById('info').appendChild(geocoder.onAdd('map'));
+  map.on('mousemove', function(e) {
+    document.getElementById('info').innerHTML =
+      // e.point is the x, y coordinates of the mousemove event relative
+      // to the top-left corner of the map
+      JSON.stringify(e.point) + '<br />' +
+      // e.lngLat is the longitude, latitude geographical position of the event
+      JSON.stringify(e.lngLat);
+  });
 
 
   map.on('load', function() {
@@ -40,7 +39,7 @@ $(document).ready(function() {
     //     url: '#lines.source.data.features.properties.color'
     // });
     map.addLayer({
-      'id': 'lines',
+      'id': 'paid',
       'type': 'line',
       'source': {
         'type': 'geojson',
@@ -73,11 +72,124 @@ $(document).ready(function() {
                   [-105.27597683690172, 40.01872613925792],
                   [-105.27216719155857, 40.01949388255309]
 
-
                 ]
               }
-            },
-            {
+              },
+              {
+                'type': 'Feature',
+                'properties': {
+                  'color': '#0433FF' // blue
+                },
+                'geometry': {
+                  'type': 'LineString',
+                  'coordinates': [
+                    [-105.28145632608522, 40.01758759440182],
+                    [-105.28647876091595, 40.016518823554804]
+                  ]
+                }
+              },
+              {
+                'type': 'Feature',
+                'properties': {
+                  'color': '#0433FF' // blue
+                },
+                'geometry': {
+                  'type': 'LineString',
+                  'coordinates': [
+                    [-105.28062926091508,40.01569653408774],
+                    [-105.2809438966849,40.016571508660576],
+                    [-105.28129076324107,40.01755654371817],
+                    [-105.28159260595864,40.018397207049134]
+                  ]
+                }
+              },{
+                'type': 'Feature',
+                'properties': {
+                  'color': '#0433FF' // blue
+                },
+                'geometry': {
+                  'type': 'LineString',
+                  'coordinates': [
+                    [-105.2805830960439,40.01566929194962],
+                    [-105.27934720497915,40.015933387514764],
+                    [-105.27804369351013,40.01619831668623]
+                  ]
+                }
+              },
+              {
+                'type': 'Feature',
+                'properties': {
+                  'color': '#0433FF' // blue
+                },
+                'geometry': {
+                  'type': 'LineString',
+                  'coordinates': [
+                    [-105.27570328231147,40.017655990459104],
+                    [-105.27542061146882,40.01677225602228]
+                  ]
+                }
+              },
+              {
+                'type': 'Feature',
+                'properties': {
+                  'color': '#0433FF' // blue
+                },
+                'geometry': {
+                  'type': 'LineString',
+                  'coordinates': [
+                    [-105.27797329167466,40.016204657645744],
+                    [-105.2754315801184,40.01673596374184]
+                  ]
+                }
+              },
+                {
+                  'type': 'Feature',
+                  'properties': {
+                    'color': '#0433FF' // blue
+                  },
+                  'geometry': {
+                    'type': 'LineString',
+                    'coordinates': [
+                      [-105.27800955195346,40.01626812644179],
+                      [-105.27806172454105,40.01640061530989],
+                      [-105.27806172454105,40.016665592280305],
+                      [-105.27822922608946,40.016878444420655],
+                      [-105.27901340111454,40.01915336973801]
+                    ]
+                  }
+              },
+              {
+                'type': 'Feature',
+                'properties': {
+                  'color': '#0433FF' // blue
+                },
+                'geometry': {
+                  'type': 'LineString',
+                  'coordinates': [
+                    [-105.28264409267116,40.017403340994775],
+                    [-105.28295515524904,40.018309319531]
+                  ]
+                }
+            }
+            ]
+          }
+
+        },
+              'paint': {
+                'line-width': 2,
+                // Use a get expression (https://www.mapbox.com/mapbox-gl-js/style-spec/#expressions-get)
+                // to set the line-color to a feature property value.
+                'line-color': ['get', 'color']
+              }
+            });
+    map.addLayer({
+      'id': 'lines',
+      'type': 'line',
+      'source': {
+        'type': 'geojson',
+        'data': {
+          'type': 'FeatureCollection',
+          'features': [{
               'type': 'Feature',
               'properties': {
                 'color': '#00FDFF' // light blue
@@ -87,6 +199,7 @@ $(document).ready(function() {
                 'coordinates': [
                   [-105.28371010032444, 40.016032557560465],
                   [-105.2859144330055, 40.01557317635502]
+
                 ]
               }
             }, {
@@ -130,6 +243,8 @@ $(document).ready(function() {
                   [-105.28297767585381, 40.011936971134276],
                   [-105.27837480134144, 40.01285715039489],
                   [-105.28418703230207, 40.011659304504406],
+                  [-105.29147012605384,40.01169327310822],
+                  [-105.28418703230207, 40.011659304504406],
                   [-105.28721432653008, 40.011664744848825],
                   [-105.28718589263214, 40.01289518856575]
                 ]
@@ -144,21 +259,51 @@ $(document).ready(function() {
                 'coordinates': [
                   [-105.28531789555299, 40.017880709836334],
                   [-105.28172208547981, 40.01863961056429],
-                  [-105.27662110557027, 40.01966032690248]
+                  [-105.27662110557027, 40.01966032690248],
+                  [-105.27007494060508,40.02102017064203]
 
                 ]
               }
-            },
-            {
+            },{
               'type': 'Feature',
               'properties': {
-                'color': '#0433FF' // blue
+                'color': '#00FDFF' // light blue
               },
               'geometry': {
                 'type': 'LineString',
                 'coordinates': [
-                  [-105.28145632608522, 40.01758759440182],
-                  [-105.28647876091595, 40.016518823554804]
+                  [-105.28670870331922,40.0165513867224],
+                  [-105.2914906544681,40.015558747981686]
+
+
+                ]
+              }
+            },{
+              'type': 'Feature',
+              'properties': {
+                'color': '#00FDFF' // light blue
+              },
+              'geometry': {
+                'type': 'LineString',
+                'coordinates': [
+                  [-105.29146096851576,40.015480178673215],
+                  [-105.28669937136652,40.01645306002197]
+
+
+                ]
+              }
+            },{
+              'type': 'Feature',
+              'properties': {
+                'color': '#00FDFF' // light blue
+              },
+              'geometry': {
+                'type': 'LineString',
+                'coordinates': [
+                  [-105.28651906249193,40.01649591125303],
+                  [-105.2862448809349,40.01562140623372]
+
+
                 ]
               }
             },
@@ -171,7 +316,91 @@ $(document).ready(function() {
                 'type': 'LineString',
                 'coordinates': [
                   [-105.28121841344827,40.01761392603933],
-                  [-105.27609755897231,40.01868721212625]
+                  [-105.27609755897231,40.01868721212625],
+                  [-105.27577831828472,40.01786552696984]
+
+
+
+                ]
+              }
+            },
+            {
+              'type': 'Feature',
+              'properties': {
+                'color': '#FF2600' // red
+              },
+              'geometry': {
+                'type': 'LineString',
+                'coordinates': [
+                  [-105.2836929521471,40.016568877932656],
+                  [-105.27592039128209,40.01818307487474],
+
+
+
+                ]
+              }
+            },
+            {
+              'type': 'Feature',
+              'properties': {
+                'color': '#FF2600' // red
+              },
+              'geometry': {
+                'type': 'LineString',
+                'coordinates': [
+                  [-105.27797329167466,40.016204657645744],
+                  [-105.27628024452514, 40.016550373083476]
+
+
+
+                ]
+              }
+            },
+            {
+              'type': 'Feature',
+              'properties': {
+                'color': '#FF2600' // red
+              },
+              'geometry': {
+                'type': 'LineString',
+                'coordinates': [
+                  [-105.27928974262802,40.01596401434014],
+                  [-105.28030305278686,40.01886951875579]
+
+
+
+                ]
+              }
+            },
+            {
+              'type': 'Feature',
+              'properties': {
+                'color': '#FF2600' // red
+              },
+              'geometry': {
+                'type': 'LineString',
+                'coordinates': [
+                  [-105.28319429353674,40.015130166409165],
+                  [-105.28067862348745,40.015648301794954]
+
+
+
+                ]
+              }
+            },
+            {
+              'type': 'Feature',
+              'properties': {
+                'color': '#FF2600' // red
+              },
+              'geometry': {
+                'type': 'LineString',
+                'coordinates': [
+                  [-105.28322151681448,40.01495645989456],
+                  [-105.28343285157256,40.01406641337695],
+                  [-105.28348196508868,40.01354293123646],
+                  [-105.2831799359197,40.01261842180932]
+
 
 
                 ]
